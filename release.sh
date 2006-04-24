776d9dc3c079adb4fa6efea4824f990d231deb04
Luis Oliveira <loliveira@common-lisp.net>
2006-04-24 02:14:27 +0100

    new file: release.sh
    darcs-hash:20060424011427-28748-3d7d772848d19646d484df4c97a09b357faf5b92.gz

---
+++ release.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+VERSION=${VERSION:=`cat trivial-garbage.asd  | grep :version | sed -e "s/.*\"\(.*\)\".*/\1/"`}
+TARBALL_NAME="trivial-garbage_$VERSION"
+TARBALL="$TARBALL_NAME.tar.gz"
+SIGNATURE="$TARBALL.asc"
+RELEASE_DIR=${RELEASE_DIR:="public_html/tarballs/"}
+
+echo "Creating distribution..."
+darcs dist -d "$TARBALL_NAME"
+
+echo "Signing tarball..."
+gpg -b -a "$TARBALL_NAME.tar.gz"
+
+echo "Copying tarball to web server..."
+#scp "$TARBALL" "$SIGNATURE" common-lisp.net:"$RELEASE_DIR"
+
+echo "Uploaded $TARBALL and $SIGNATURE."
+echo "Don't forget to update the link on the CLiki page!"
